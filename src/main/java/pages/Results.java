d0f9ecad71f16b84e1dc581890b01032ea89cc38
Ahmed Gomaa <ahmed.1.gomaa7@gmail.com>
2024-09-10 12:17:37 +0300

    Task 3 Final

--- src/main/java/pages/Results.java
+++ src/main/java/pages/Results.java
@@ -1,5 +1,17 @@
 package pages;
 
-public class Results {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 
+public class Results {
+    WebDriver driver;
+    By firstLink = By.xpath("//article[@id = 'r1-0']//a[@data-testid= 'result-extras-url-link']");
+
+    public Results(WebDriver driver) {
+        this.driver = driver;
+    }
+    public String getFirstResultLink(){
+        return driver.findElement(firstLink).getAttribute("href");
+    }
 }
+
--- src/test/java/Modular/Task3Test.java
+++ src/test/java/Modular/Task3Test.java
@@ -1,11 +1,20 @@
 package Modular;
 
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.NotFoundException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pages.Landing;
+import pages.Results;
+
+import java.time.Duration;
 
 public class Task3Test {
     /*
@@ -18,12 +27,20 @@ public class Task3Test {
     * */
     WebDriver driver;
     Landing home;
-
+    Results results;
+    Wait<WebDriver> wait;
     @BeforeTest
     public void beforeTest(){
         driver = new ChromeDriver();
         home = new Landing(driver);
         home.navigate();
+        results = new Results(driver);
+        wait = new FluentWait<>(driver).withTimeout(Duration.ofSeconds(3))
+                .pollingEvery(Duration.ofMillis(300))
+                .ignoring(NotFoundException.class)
+                .ignoring(ElementNotInteractableException.class)
+                .ignoring(StaleElementReferenceException.class)
+                .ignoring(AssertionError.class);
     }
     @AfterTest
     public void afterTest(){
@@ -34,5 +51,13 @@ public class Task3Test {
     public void assertLinkOfTheFirstResult(){
         String searchQuery = "Selenium WebDriver";
         home.search(searchQuery);
+        wait.until(
+                d -> {
+                    var firstLink = results.getFirstResultLink();
+                    Assert.assertEquals(firstLink, "https://www.selenium.dev/documentation/webdriver/");
+                    return true;
+                });
+
+
     }
 }
